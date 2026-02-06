4a2b5d662fc5468660305b7aca15caebf7299bf4
o547 <otashibaura@gmail.com>
2026-02-06 22:22:23 +0900

    b

--- yahoo_finance_up_down_lstm.py
+++ yahoo_finance_up_down_lstm.py
@@ -11,52 +11,50 @@ import pandas as pd
 import numpy as np
 from torch.utils.data import DataLoader, Dataset
 # import scipy.stats
+import matplotlib.pyplot as plt
 
 
 
 # csvからimport
-df = pd.read_csv('./21days.csv',  header=0)
+df = pd.read_csv('./65days.csv',  header=0)
+my_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-all_datas=df.iloc[0:, 0:20].to_numpy().astype(np.float32)
-all_labels=df.iloc[0:, 20].to_numpy().astype(np.float32)
-print(all_datas)
-
+all_datas=df.iloc[0:, 0:64].to_numpy().astype(np.float32)
+all_labels=df.iloc[0:, 64].to_numpy().astype(np.float32)
 
 for i in range(len(all_labels)):
-    if(all_labels[i]>all_datas[i][19]):
+    if(all_labels[i]>all_datas[i][63]):
         all_labels[i]=1 #上がる
     else:
         all_labels[i]=0 #下がる
 
 all_datas = np.log(all_datas[:, 1:] / all_datas[:, :-1])
+all_datas=torch.from_numpy(all_datas)
+all_labels=torch.from_numpy(all_labels)
 print(all_datas)
 
 
-datas = all_datas[::2]
-labels = all_labels[::2]
+datas = all_datas[::2].to(my_device)
+labels = all_labels[::2].to(my_device)
 
 
-test_datas = all_datas[1::2]
-test_labels = all_labels[1::2]
-
+test_datas = all_datas[1::2].to(my_device)
+test_labels = all_labels[1::2].to(my_device)
+print("データインポート完了")
 
 
 
 # カスタムデータセットの作成
 class CustomDataset(Dataset):
     def __init__(self, datas, labels):
-        # データとラベルを受け取るコンストラクタ
         self.datas = datas
         self.labels = labels
 
     def __len__(self):
-        # データセットのサイズを返す
         return len(self.datas)
 
     def __getitem__(self, idx):
-        # 指定したインデックスのデータとラベルを返す
-        sample = {'datas': self.datas[idx], 'label': self.labels[idx]}
-        return sample
+        return self.datas[idx], self.labels[idx]
 
 
 
@@ -66,35 +64,41 @@ test_dataset = CustomDataset(test_datas, test_labels)
 
 
 # DataLoaderの作成
-data_loader = DataLoader(dataset=custom_dataset, batch_size=1000, shuffle=True)
-test_loader = DataLoader(dataset=test_dataset, batch_size=1000, shuffle=True)
+data_loader = DataLoader(dataset=custom_dataset, batch_size=4000, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=4000, shuffle=True)
 
 
 class mlp_net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.lstm = nn.LSTM(1, 16, batch_first=True)
+        self.lstm = nn.LSTM(1, 64, num_layers=2, batch_first=True)
+        self.dropout = nn.Dropout(0.2)
+        self.fc = nn.Linear(64, 32)
+        self.fc2 = nn.Linear(32, 2)
 
-        self.fc = nn.Linear(16, 2)
         self.criterion = nn.CrossEntropyLoss()
         self.optimizer = optim.Adam(self.parameters())
 
     def forward(self, x):
         _, (h_n, _) = self.lstm(x)
         x = h_n[-1]
+
         x = self.fc(x)
+        x = F.relu(x)
+        x=self.dropout(x)
+
+
+        x = self.fc2(x)
         return x
 
 
 def train(model, train_loader):
-    # 今は学習時であることを明示するコード
     model.train()
-    for i in range(10):
+
+    for i in range(300):
         # ミニバッチごとにループさせる,train_loaderの中身を出し切ったら1エポックとなる
-        for batch in train_loader:
-            batch_items=batch['datas']
-            batch_items = batch_items.reshape(-1, 19, 1)
-            batch_labels=batch['label']
+        for batch_items, batch_labels  in train_loader:
+            batch_items = batch_items.view(-1, 63, 1)
             batch_labels = batch_labels.long()
             outputs = model(batch_items)  # 順伝播
             model.optimizer.zero_grad()  # 勾配を初期化（前回のループ時の勾配を削除）
@@ -107,7 +111,6 @@ def train(model, train_loader):
 
 
 def test(model, train_loader):
-    # 今は学習時であることを明示するコード
     model.eval()
 
     # 正しい予測数、損失の合計、全体のデータ数を数えるカウンターの0初期化
@@ -115,11 +118,8 @@ def test(model, train_loader):
     total_loss = 0
     total_data_len = 0
 
-    # ミニバッチごとにループさせる,train_loaderの中身を出し切ったら1エポックとなる
-    for batch in train_loader:
-        batch_items=batch['datas']
-        batch_items = batch_items.reshape(-1, 19, 1)
-        batch_labels=batch['label']
+    for batch_items, batch_labels  in train_loader:
+        batch_items = batch_items.view(-1, 63, 1)
         batch_labels = batch_labels.long()
         outputs = model(batch_items)  # 順伝播
         loss = model.criterion(outputs, batch_labels)  # 損失を計算
@@ -128,30 +128,38 @@ def test(model, train_loader):
         _, pred_labels = torch.max(outputs, axis=1)  # outputsから必要な情報(予測したラベル)のみを取り出す。
         batch_size = len(batch_labels)  # バッチサイズの確認
         for i in range(batch_size):  # データ一つずつループ,ミニバッチの中身出しきるまで
-            total_data_len += 1  # 全データ数を集計
-            if pred_labels[i] == batch_labels[i]:
-                total_correct += 1 # 正解のデータ数を集計
-            if(total_data_len%10000==0):
-                print("data:"+str(batch_items[i])+"  out:"+str(outputs[i])+"  label:"+str(batch_labels[i]))
+
+            outputs_softmax=softmax=F.softmax(outputs[i], dim=0)
+            if outputs_softmax[0].item()>0.6 or outputs_softmax[1].item()>0.6:
+
+
+                total_data_len += 1  # 全データ数を集計
                 if pred_labels[i] == batch_labels[i]:
-                    print("正解")
-                else:
-                    print("不正解")
+                    total_correct += 1 # 正解のデータ数を集計
+                if(total_data_len%10000==0):
+                    print("out:"+str(outputs_softmax)+"  label:"+str(batch_labels[i]))
+                    if pred_labels[i] == batch_labels[i]:
+                        print("正解")
+                    else:
+                        print("不正解")
         total_loss += loss.item()  # 全損失の合計
 
-    # 今回のエポックの正答率と損失を求める
+    #正答率と損失を求める
     accuracy = total_correct/total_data_len*100  # 予測精度の算出
     loss = total_loss/total_data_len  # 損失の平均の算出
     return accuracy, loss
 
+
+
 # モデルを宣言する
-model = mlp_net()
+model = mlp_net().to(my_device)
+model.load_state_dict(torch.load('up_down_model_weight_100.pth', map_location=my_device))
 
 # 学習させ、その結果を表示する
-train(model, data_loader)
-torch.save(model.state_dict(), 'up_down_model_weight.pth')
+# train(model, data_loader)
+torch.save(model.state_dict(), 'up_down_model_weight_100.pth')
 model_scripted = torch.jit.script(model)
-model_scripted.save('up_down_model_scripted.pth')
+model_scripted.save('up_down_model_scripted_100.pth')
 
 acc, loss = test(model, test_loader)
 print(f'正答率: {acc}, 損失: {loss}')
